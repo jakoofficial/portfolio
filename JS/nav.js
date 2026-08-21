22ed68084f0fe4af5e189f58add4151046a17ff8
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-08-21 12:20:59 +0200

    Moved JS from index to own nav file

---
+++ JS/nav.js
@@ -0,0 +1,20 @@
+// let navOpen = true;
+const navActionButton = document.getElementById("navActionButton");
+const navigation = document.getElementById("navigation");
+
+navActionButton.addEventListener("click", () => {
+  navigation.classList.toggle("nav_close");
+});
+
+const pagesArr = [
+  "Pages/welcome.html",
+  "Pages/about.html",
+  "Pages/projects.html",
+];
+$(document).ready(function () {
+  let i = 0;
+  while (i < pagesArr.length) {
+    $("#container").append($("<section>").load(pagesArr[i]));
+    i++;
+  }
+});
